8ecb43c126eee4742dddb4ddf1d2704c3d32740a
longyan <longyan@nit.zju.edu.cn>
2022-06-28 16:24:32 +0800

    Create 1028.cpp

---
+++ BasicLevel/1028.cpp
@@ -0,0 +1,56 @@
+/*
+某城镇进行人口普查，得到了全体居民的生日。现请你写个程序，找出镇上最年长和最年轻的人。
+这里确保每个输入的日期都是合法的，但不一定是合理的——假设已知镇上没有超过 200 岁的老人，而今天是 2014 年 9 月 6 日，所以超过 200 岁的生日和未出生的生日都是不合理的，应该被过滤掉。
+
+输入格式：
+输入在第一行给出正整数 N，取值在(0,105]；随后 N 行，每行给出 1 个人的姓名（由不超过 5 个英文字母组成的字符串）、以及按 yyyy/mm/dd（即年/月/日）格式给出的生日。题目保证最年长和最年轻的人没有并列。
+
+输出格式：
+在一行中顺序输出有效生日的个数、最年长人和最年轻人的姓名，其间以空格分隔。
+*/
+
+// @Author:longyan
+// @Date:2022/6/28
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+struct people
+{
+	string name;
+	string birthday;
+};
+
+int cmp(people a, people b)
+{
+	return a.birthday < b.birthday;
+}
+
+int main()
+{
+	int N;
+	cin >> N;
+	int cnt = 0;
+	vector<people> MaxMin;
+	const string MaxBirthday = "1814/09/06";
+	const string MinBirthday = "2014/09/06";
+	for (int i = 0; i < N; i++)
+	{
+		people person;
+		cin >> person.name >> person.birthday;
+		if (person.birthday >= MaxBirthday && person.birthday <= MinBirthday)
+		{
+			cnt++;
+			MaxMin.push_back(person);
+		}
+	}
+
+	sort(MaxMin.begin(), MaxMin.end(), cmp);
+
+	cout << cnt << " " << MaxMin[0].name << " " << MaxMin.back().name << endl;
+
+	return 0;
+}
