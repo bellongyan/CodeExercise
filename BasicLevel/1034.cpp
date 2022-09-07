5cd4df79cd98d173b71c571e0b643302c6c83bc8
longyan <longyan@nit.zju.edu.cn>
2022-09-08 00:51:49 +0800

    Update 1034.cpp

--- BasicLevel/1034.cpp
+++ BasicLevel/1034.cpp
@@ -19,19 +19,16 @@ using namespace std;
 long long a, b, c, d;
 
 // 求最大公约数
-long long gcd(long long t1, long long t2)
-{
+long long gcd(long long t1, long long t2) {
 	return t2 == 0 ? t1 : gcd(t2, t1 % t2);
 }
 
 // 显示函数
-void func(long long m, long long n)
-{
+void func(long long m, long long n) {
 	// 如果 n 等于 0 ，则输出 inf, 否则输出 0
-	if (m * n == 0)
-	{
+	if (m * n == 0) {
 		printf("%s", n == 0 ? "Inf" : "0");
-		return;
+		return ;
 	}
 
 	// 设置 ‘-’ 符号，当 m, n 中有一个为负数为 true
@@ -45,39 +42,35 @@ void func(long long m, long long n)
 
 	// 如果 flag 为 true 那么输出 "(-" 不然输出空字符串
 	printf("%s", flag ? "(-" : "");
-
+	
 	// 如果整数部分不为 0 就输出整数部分
-	if (x != 0)
-		printf("%lld", x);
-
+	if (x != 0) printf("%lld", x);
+	
 	// m 对 n 取余，如果为 0 再判断 flag 是否为 true, 是的话输出 ")" 补全括号 函数结束
-	if (m % n == 0)
-	{
-		if (flag)
-			printf(")");
-		return;
+	if (m % n == 0) {
+		if (flag) printf(")");
+		return ;
 	}
-
+	
 	// 如果整数部分为 0 输出空字符串
-	if (x != 0)
-		printf(" ");
-
+	if (x != 0) printf(" ");
+	
 	// m 重新赋值
 	m = m - x * n;
-
+	
 	// 计 t 为 m, n 的最小公约数
 	long long t = gcd(m, n);
-
+	
 	// m, n 分别除最小公约数
 	m = m / t;
 	n = n / t;
-
+	
 	// 对得到的 m, n 进行输出
 	printf("%lld/%lld%s", m, n, flag ? ")" : "");
 }
 
-int main()
-{
+
+int main() {
 	scanf("%lld/%lld %lld/%lld", &a, &b, &c, &d);
 	func(a, b);
 	printf(" + ");
