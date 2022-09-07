8df598fa0a6ad1fa094c7337456349fa2f586baa
longyan <longyan@nit.zju.edu.cn>
2022-09-08 00:52:24 +0800

    Update 1043.cpp

--- BasicLevel/1043.cpp
+++ BasicLevel/1043.cpp
@@ -11,31 +11,22 @@
 // @Author:longyan
 // @Date:2022/7/19
 
-#include <iostream>
+#include<iostream>
 
 using namespace std;
 
-int main()
-{
+int main() {
 	int m[128] = {0};
 	string s;
 	getline(cin, s);
-	for (char c : s)
-		m[c]++;
-	while (m['P'] > 0 || m['T'] > 0 || m['A'] > 0 || m['t'] > 0 || m['e'] > 0 || m['s'] > 0)
-	{
-		if (m['P']-- > 0)
-			cout << "P";
-		if (m['A']-- > 0)
-			cout << "A";
-		if (m['T']-- > 0)
-			cout << "T";
-		if (m['e']-- > 0)
-			cout << "e";
-		if (m['s']-- > 0)
-			cout << "s";
-		if (m['t']-- > 0)
-			cout << "t";
+	for (char c : s) m[c]++;
+	while (m['P'] > 0 || m['T'] > 0 || m['A'] > 0 || m['t'] > 0 || m['e'] > 0 || m['s'] > 0) {
+		if (m['P']-- > 0) cout << "P";
+		if (m['A']-- > 0) cout << "A";
+		if (m['T']-- > 0) cout << "T";
+		if (m['e']-- > 0) cout << "e";
+		if (m['s']-- > 0) cout << "s";
+		if (m['t']-- > 0) cout << "t";
 	}
 
 	return 0;
