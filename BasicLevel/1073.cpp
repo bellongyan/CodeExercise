002ba7b76c4aefbef58d3009bc2c5817ab136270
longyan <longyan@nit.zju.edu.cn>
2023-05-13 00:52:23 +0800

    Update 1073.cpp

--- BasicLevel/1073.cpp
+++ BasicLevel/1073.cpp
@@ -1,54 +1,58 @@
-/*
-
- */
-
-// @Author:longyan
-// @Date:2023-05-12
-
 #include <iostream>
 #include <vector>
-
+#include <cmath>
 using namespace std;
-
-struct Problem {
-	int score;
-	int select;
-	int correct;
-	vector<string> answer;
-};
-
 int main() {
-	int N, M;
-	cin >> N >> M;
-	vector<Problem> problemSet;
-	for (int i = 0; i < M; ++i) {
-		Problem p;
-		cin >> p.score;
-		cin >> p.select;
-		cin >> p.correct;
-		for (int j = 0; j < p.correct; ++j) {
-			string a;
-			cin >> a;
-			p.answer.push_back(a);
+	int n, m, optnum, truenum, temp, maxcnt = 0;
+	int hash[] = {1, 2, 4, 8, 16}, opt[1010][110] = {0};
+	char c;
+	scanf("%d %d", &n, &m);
+	vector<int> fullscore(m), trueopt(m);
+	vector<vector<int>> cnt(m, vector<int>(5));
+	for (int i = 0; i < m; i++) {
+		scanf("%d %d %d", &fullscore[i], &optnum, &truenum);
+		for (int j = 0; j < truenum; j++) {
+			scanf(" %c", &c);
+			trueopt[i] += hash[c - 'a'];
 		}
-		problemSet.push_back(p);
 	}
-
-	vector<vector<string>> answerSet;
-	for (int i = 0; i < N; ++i) {
-		vector<string> answer;
-		for (int  j = 0; j < M; ++j) {
-			int n;
-			scanf("(%d", &n);
-			for (int k; k < n; k++) {
-				string as;
-				cin >> as;
-				answer.push_back(as);
+	for (int i = 0; i < n; i++) {
+		double grade = 0;
+		for (int j = 0; j < m; j++) {
+			getchar();
+			scanf("(%d", &temp);
+			for (int k = 0; k < temp; k++) {
+				scanf(" %c)", &c);
+				opt[i][j] += hash[c - 'a'];
 			}
-			scanf(")");
+			int el = opt[i][j] ^ trueopt[j];
+			if (el) {
+				if ((opt[i][j] | trueopt[j]) == trueopt[j]) {
+					grade += fullscore[j] * 1.0 / 2;
+				}
+				if (el) {
+					for (int k = 0; k < 5; k++)
+						if (el & hash[k]) cnt[j][k]++;
+				}
+			} else {
+				grade += fullscore[j];
+			}
 		}
-		answerSet.push_back(answer);
+		printf("%.1f\n", grade);
 	}
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < 5; j++)
+			maxcnt = maxcnt > cnt[i][j] ? maxcnt : cnt[i][j];
 
+	if (maxcnt == 0) {
+		printf("Too simple\n");
+	} else {
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < cnt[i].size(); j++) {
+				if (maxcnt == cnt[i][j])
+					printf("%d %d-%c\n", maxcnt, i + 1, 'a' + j);
+			}
+		}
+	}
 	return 0;
 }
